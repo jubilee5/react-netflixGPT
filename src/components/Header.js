c7e5e8b8498aab98f465b8a9760320799dff0887
jubilee5 <jubileesharma17@gmail.com>
2026-05-31 22:00:29 +0530

    fixed bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
   const handleSignOut = () => {
     signOut(auth)           //took this code from firebase docs
     .then(() => {
@@ -21,14 +23,17 @@ const Header = () => {
     src = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-05-14/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
     alt="logo" />
 
+    {user && (
+
     <div className="flex p-2">
       <img
       className="w-12 h-12  cursor-pointer "
       alt="userIcon"
-      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+      src={user?.photoURL}
       />    
       <button onClick={handleSignOut} className="font-bold text-white">(Sign Out)</button>
       </div>
+    )}
     </div>
   )
 }
