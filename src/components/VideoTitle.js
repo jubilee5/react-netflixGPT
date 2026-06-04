1f654d988f88d792bfd3938cfa8e87edd04f5087
jubilee5 <jubileesharma17@gmail.com>
2026-06-04 23:57:11 +0530

    added css to the video title

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,9 +2,13 @@
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{overview}</p>
+    <div className="pt-36 px-12">
+      <h1 className="text-6xl font-bold mb-3">{title}</h1>
+      <p className=" py-6 text-lg w-1/4">{overview}</p>
+      <div className="flex gap-4">
+        <button className="bg-gray-500 text-white px-12 py-4 rounded-md font-bold bg-opacity-75 hover:bg-gray-600"> ▶ Play</button>
+        <button className="bg-gray-500 text-white px-12 py-4 rounded-md font-bold bg-opacity-75 hover:bg-gray-600">More Info</button>
+      </div>
     </div>
   )
 }
