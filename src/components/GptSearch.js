c45706e48dde304cd822ecec59e4d94398162734
jubilee5 <jubileesharma17@gmail.com>
2026-06-08 23:59:32 +0530

    enhanced GPT search bar

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -4,6 +4,13 @@ import GptMovieSuggestions from "./GptMovieSuggestions";
 const GptSearch = () => {
   return (
     <div>
+         <div className="absolute -z-10">
+
+      <img
+        className="w-screen h-screen object-cover"
+        src= "https://assets.nflxext.com/ffe/siteui/vlv3/435e8bb8-7f1b-49cb-8da8-bff997124294/web/IN-en-20260511-TRIFECTA-perspective_ec39852e-0b48-4e8a-b415-dd8376cd83ce_large.jpg" alt="background"
+        />
+      </div>
       <GptSearchBar/>
       <GptMovieSuggestions/>
     </div>
--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const GptSearchBar = () => {
   return (
-    <div>
-      
+    <div className='pt-[10%] flex justify-center'>
+      <form className="w-1/2 bg-black grid grid-cols-12">
+        <input 
+        type="text" 
+        className=" p-4 m-4 col-span-9"
+        placeholder="Search for movies, TV shows, genres..."/>
+        <button className="col-span-3 m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button>
+        </form>
     </div>
   )
 }
