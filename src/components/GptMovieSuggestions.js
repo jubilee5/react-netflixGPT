6831343d3aa6d2e411952981f88da25e7c201004
jubilee5 <jubileesharma17@gmail.com>
2026-06-17 16:40:25 +0530

    implemented the search result in the ui

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const GptMovieSuggestions = () => {
+  const { movieResults, movieNames } = useSelector((store) => store.gpt);
+  if (!movieNames) return null;
   return (
-    <div>
-      
+    <div className="p-4 m-4 bg-black text-white">
+      {movieNames.map((movieNames, index) => (
+        <MovieList
+          key={movieNames}
+          title={movieNames}
+          movies={movieResults[index]}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default GptMovieSuggestions
+export default GptMovieSuggestions;
