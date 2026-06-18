1ede0898d015b7704959265a40fa35797f559e60
jubilee5 <jubileesharma17@gmail.com>
2026-06-18 23:15:54 +0530

    enhanced the ui of Gpt search page

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -5,7 +5,7 @@ const GptMovieSuggestions = () => {
   const { movieResults, movieNames } = useSelector((store) => store.gpt);
   if (!movieNames) return null;
   return (
-    <div className="p-4 m-4 bg-black text-white">
+    <div className="p-4 m-4 bg-black text-white bg-opacity-90">
       {movieNames.map((movieNames, index) => (
         <MovieList
           key={movieNames}
--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -5,10 +5,10 @@ import { BG_URL } from "../utils/constants";
 const GptSearch = () => {
   return (
     <div>
-         <div className="absolute -z-10">
+         <div className="fixed -z-10">
 
       <img
-        className="w-screen h-screen object-cover"
+        className=""
         src= {BG_URL} alt="background"
         />
       </div>
